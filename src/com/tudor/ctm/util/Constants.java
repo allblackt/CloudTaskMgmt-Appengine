4d13384dc9878666941bb91409ff3bd5ffb361ca
allblackt <allblack007@gmail.com>
2013-07-15 12:09:37 +0300

    Update Constants.java

--- src/com/tudor/ctm/util/Constants.java
+++ src/com/tudor/ctm/util/Constants.java
@@ -2,5 +2,5 @@ package com.tudor.ctm.util;
 
 public class Constants {
 
-	public static final String GCM_API_KEY = "AIzaSyBzZ78cbkE7T6OxPer7hQVCUeDEJWL-wbg";
+	public static final String GCM_API_KEY = "<YOUR KEY HERE>";
 }
